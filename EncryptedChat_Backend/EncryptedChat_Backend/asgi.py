cc8997bf3dbc480aa73193aeaf88d1a17bb35904
AlexReyes03 <20233tn160@utez.edu.mx>
2026-03-05 12:48:29 -0600

    [CHORE] Configuración del proyecto y dependencias

--- EncryptedChat_Backend/EncryptedChat_Backend/asgi.py
+++ EncryptedChat_Backend/EncryptedChat_Backend/asgi.py
@@ -8,9 +8,20 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
 """
 
 import os
-
 from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from apps.users.middleware import JWTAuthMiddlewareStack
+import apps.chat.routing
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'EncryptedChat_Backend.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "EncryptedChat_Backend.settings")
 
-application = get_asgi_application()
+django_asgi_app = get_asgi_application()
+
+application = ProtocolTypeRouter(
+    {
+        "http": django_asgi_app,
+        "websocket": JWTAuthMiddlewareStack(
+            URLRouter(apps.chat.routing.websocket_urlpatterns)
+        ),
+    }
+)
