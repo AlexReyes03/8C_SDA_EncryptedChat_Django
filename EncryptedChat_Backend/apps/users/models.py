182868715e647db369769eacf174f2a7abebc200
AlexReyes03 <20233tn160@utez.edu.mx>
2026-03-08 00:59:52 -0600

    [CHORE] Ajuste de dependencias NPM, config de gitignore y modelo base de Usuarios

--- EncryptedChat_Backend/apps/users/models.py
+++ EncryptedChat_Backend/apps/users/models.py
@@ -1,8 +1,28 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.core.validators import RegexValidator
+
+
+class AllowedSpacesUsernameValidator(RegexValidator):
+    regex = r"^[\w.@+ -]+$"
+    message = "Ingrese un nombre de usuario válido. Este puede contener sólo letras, números, espacios y caracteres @/./+/-/_."
+    flags = 0
 
 
 class User(AbstractUser):
+    username_validator = AllowedSpacesUsernameValidator()
+
+    username = models.CharField(
+        "username",
+        max_length=150,
+        unique=True,
+        help_text="Requerido. 150 caracteres o menos. Letras, dígitos, espacios y @/./+/-/_ permitidos.",
+        validators=[username_validator],
+        error_messages={
+            "unique": "Ya existe un usuario con ese nombre.",
+        },
+    )
+
     # AbstractUser gives us username, first_name, last_name, email, password
     # We can add a simple profile picture or bio if needed in the future
     created_at = models.DateTimeField(auto_now_add=True)
